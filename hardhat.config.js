118ef6dd49bbf8ce6ea24595ee3369a660933fea
litmus-zhang <40471908+litmus-zhang@users.noreply.github.com>
2022-03-26 14:30:48 +0100

    dev dependency added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,8 +21,9 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/417dlzcu7_Dlqje6nniTXPav9CkEaN5z",
-      accounts: ["79e220462ca7d848d5b89a743f831c8fc0cdfd430f0ac0b2dfdb4e6ace4e4131"]
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY]
     },
+    
   },
 };
